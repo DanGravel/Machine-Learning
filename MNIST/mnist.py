b5d0f2e2d6530f9a105e3bb22227634d582557ae
DanGravel <dan0gravel@gmail.com>
2016-06-21 12:49:52 -0400

    Added classify user  image

--- MNIST/mnist.py
+++ MNIST/mnist.py
@@ -14,30 +14,32 @@ def loadData():
 	
 	#Data is stored in tuple in 3 lists
 	X_train, y_train = data[0]
-	X_val, y_val = data[1]
+	X_validation, y_validation = data[1]
 	X_test, y_test = data[2]
 
 	#Make the training data into 28x28 matrix
 	X_train = X_train.reshape((-1,1,28,28))
-	X_val   = X_val.reshape((-1,1,28,28))
+	X_validation   = X_validation.reshape((-1,1,28,28))
 	X_test  = X_test.reshape((-1,1,28,28))
 
 	#Set the y values to unsigned integers(uint8)
 	y_train = y_train.astype(np.uint8) 
-	y_val   = y_val.astype(np.uint8)
+	y_validation   = y_validation.astype(np.uint8)
 	y_test  = y_test.astype(np.uint8)
 
-	return {'X_train':X_train, 'X_val':X_val, 'X_test':X_test,
-			'y_train':y_train, 'y_val':y_val, 'y_test':y_test}
+	return {'X_train':X_train, 'X_validation':X_validation, 'X_test':X_test,
+			'y_train':y_train, 'y_validation':y_validation, 'y_test':y_test}
 
 """
 Accuracy is about 95%
 """
 def trainNetwork():
-	loadedData = loadData()
-	target = loadedData["y_train"]
+	trainData = loadData()
+	
+	target = trainData["y_train"]
 	features = []
 	test = []
+	
 	#unrolls the 2d matrix to a vecor to be used as inputs for classifier
 	for i in range(loadedData["y_train"].size):
 		features.append(loadedData["X_train"][i][0].ravel())
@@ -46,10 +48,13 @@ def trainNetwork():
 
 	clf = RandomForestClassifier(n_estimators = 10, n_jobs = 2)
 	clf = clf.fit(features, target)
+	
 	return clf
-	#print(clf.score(test, loadedData["y_test"]))
-
 
 def showImg(image):
 	plt.imshow(image , cmap= cm.binary)
 	plt.show()
+
+def classifyImg(image, network):
+	image = imread("image", flatten = True)
+	return network.predict(image.ravel())
